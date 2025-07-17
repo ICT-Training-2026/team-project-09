ba5b90cde6bab621256054df8d2306b2dae6d8f7
github Takashi-Horihata <github Takashi-Horihata@kccs.co.jp>
2025-07-17 13:47:15 +0900

    初回コミット

---
+++ KintaiTeam09/src/main/java/com/example/demo/KintaiTeam09Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KintaiTeam09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KintaiTeam09Application.class, args);
+	}
+
+}
