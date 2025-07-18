476e28da5b1fd63d1df3cc123ecce3332bea4b4a
github Shin-Kuroda25 <shin-kuroda@kccs.co.jp>
2025-07-18 11:24:01 +0900

    「トップメニューの登録、検索・編集、エクスポートのボタンを押して画面遷移する」を作成しました。まだまだ未完成です。

--- KintaiTeam09/src/main/java/com/example/demo/controller/TopmenuController.java
+++ KintaiTeam09/src/main/java/com/example/demo/controller/TopmenuController.java
@@ -1,5 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TopmenuController {
 
+	@GetMapping("/regist")
+    public String regist() {
+        return "regist"; //regist.html(仮称)を表示
+    }
+
+	@GetMapping("/edit")
+    public String edit() {
+        return "edit"; //edit.html(仮称)を表示
+    }
+	
+	@GetMapping("/export")
+    public String export() {
+        return "export"; //export(仮称)を表示
+    }
+	
 }
