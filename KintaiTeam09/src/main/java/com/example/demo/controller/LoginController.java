4ffdf554eeb198255d19ac78724873bd233b9a1b
github Takashi-Horihata <github Takashi-Horihata@kccs.co.jp>
2025-07-18 14:03:33 +0900

    コメント追加

--- KintaiTeam09/src/main/java/com/example/demo/controller/LoginController.java
+++ KintaiTeam09/src/main/java/com/example/demo/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 	
+	// コメント
 	@GetMapping("/login")
 	public String login() {
 	    return "login"; //ログイン画面に遷移
