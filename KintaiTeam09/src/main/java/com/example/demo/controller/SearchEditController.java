85621d51cd1d5abadce3d234667e5ffc9aecefe9
github 250450031 <taisei-akamatsu@kccs.co.jp>
2025-07-18 13:49:59 +0900

    登録・編集・検索・エクスポートcontroller追加

--- KintaiTeam09/src/main/java/com/example/demo/controller/SearchEditController.java
+++ KintaiTeam09/src/main/java/com/example/demo/controller/SearchEditController.java
@@ -1,5 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class SearchEditController {
+	@GetMapping("/topmenu")
+	public String topmenu() {
+		return "topmenu"; // メニュー画面に遷移
+	}
+
+	// 仮実装(ページ遷移のみ)
+	@GetMapping("/top")
+	public String top() {
+	    return "top"; //トップ画面に遷移
+
+	}
 
 }
