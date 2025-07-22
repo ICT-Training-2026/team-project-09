1a40a5bc6f1f3a63fe354e9d03e885ac8571be65
github Takashi-Horihata <github Takashi-Horihata@kccs.co.jp>
2025-07-22 11:00:43 +0900

    勤怠登録処理動作テスト（備考のみ）実装

--- KintaiTeam09/src/main/java/com/example/demo/controller/TopmenuController.java
+++ KintaiTeam09/src/main/java/com/example/demo/controller/TopmenuController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TopmenuController {
 
-	@GetMapping("/regist")
-    public String regist() {
-        return "regist"; //regist.html(仮称)を表示
-    }
+//	@GetMapping("/regist")
+//    public String regist(@ModelAttribute RegistForm registForm) {
+//        return "regist"; //regist.html(仮称)を表示
+//    }
 
 	@GetMapping("/edit")
     public String edit() {
--- KintaiTeam09/src/main/java/com/example/demo/form/RegistForm.java
+++ KintaiTeam09/src/main/java/com/example/demo/form/RegistForm.java
@@ -1,13 +1,21 @@
 package com.example.demo.form;
 
-//import java.sql.Date;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 import lombok.Data;
 
 @Data
 public class RegistForm {
 	
-//	private String userId;
-//	private Date date;
+	private String userId;
+	private Date date;
+	private Integer workStatus;
+	private Timestamp clockIn;
+	private Timestamp clockOut;
+	private Integer actualWorkTime;
+	private Integer BreakTime;
+	private Integer cumOverTime;
+	private String note;
 	
 }
