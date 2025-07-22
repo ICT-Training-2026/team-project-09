adc3fa5e1b957a87a8f097c1bb11ca25d7cccbe0
github Takashi-Horihata <github Takashi-Horihata@kccs.co.jp>
2025-07-22 14:25:49 +0900

    登録フォームの情報送信を実装開始（一旦コンソールに表示）

--- KintaiTeam09/src/main/java/com/example/demo/form/RegistForm.java
+++ KintaiTeam09/src/main/java/com/example/demo/form/RegistForm.java
@@ -1,7 +1,10 @@
 package com.example.demo.form;
 
-import java.sql.Date;
+
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import lombok.Data;
 
@@ -9,13 +12,27 @@ import lombok.Data;
 public class RegistForm {
 	
 	private String userId;
-	private Date date;
+//	private Date date;
+	private LocalDate date;
 	private Integer workStatus;
+
+	private LocalTime clockInTime;
+	private LocalTime clockOutTime;
 	private Timestamp clockIn;
 	private Timestamp clockOut;
+	
 	private Integer actualWorkTime;
-	private Integer BreakTime;
+	private Integer breakTime;
 	private Integer cumOverTime;
 	private String note;
+	
+	public void combineDateTime() {
+        if (date != null && clockInTime != null && clockOutTime != null) {
+            LocalDateTime clockInDateTime = LocalDateTime.of(date, clockInTime);
+            LocalDateTime clockOutDateTime = LocalDateTime.of(date, clockOutTime);
+            this.clockIn = Timestamp.valueOf(clockInDateTime);
+            this.clockOut = Timestamp.valueOf(clockOutDateTime);
+        }
+    }
 	
 }
