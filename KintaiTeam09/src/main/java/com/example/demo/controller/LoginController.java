0c269cba2f8cfdbeabab29fe09ce13891d0dc9b6
github Takashi-Horihata <github Takashi-Horihata@kccs.co.jp>
2025-07-23 15:12:37 +0900

    ログイン機能（DBと紐づけ済）実装

--- KintaiTeam09/src/main/java/com/example/demo/controller/LoginController.java
+++ KintaiTeam09/src/main/java/com/example/demo/controller/LoginController.java
@@ -14,51 +14,50 @@ import com.example.demo.service.LoginService;
 
 import lombok.RequiredArgsConstructor;
 
-
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
-	
+
 	@Autowired
-    private final LoginService loginService;
-	
+	private final LoginService loginService;
+
 	@GetMapping("/login")
 	public String login(@ModelAttribute LoginForm loginForm) {
-	    return "login"; //ログイン画面に遷移
+		return "login"; //ログイン画面に遷移
 	}
-	
-	@PostMapping("/login-post")
-    public String loginPost(@ModelAttribute LoginForm loginForm, Model model) {
-        // ログイン処理の実行
-        Login login = new Login(loginForm.getUserId(), loginForm.getPass());
-        boolean result = loginService.execute(login);
-        Employee employee = loginService.loadAccoundInfo(login);
-        
-//        System.out.println("ログイン中の社員情報:" + employee);
 
-        // ログイン処理の成否によって処理を分岐
-        if (result) { // ログイン成功時
-            // Modelにユーザー情報を追加
-//            model.addAttribute("userId", loginForm.getUserId());
-        	model.addAttribute("userId", employee.getUserId());
-        	model.addAttribute("name", employee.getName());
-        	model.addAttribute("departmentCode", employee.getDepartmentCode());
-        	model.addAttribute("numPaidHoliday", employee.getNumPaidHoliday());
-            return "top_menu";
-        } else { // ログイン失敗時
-            model.addAttribute("errorMessage", "ユーザーIDまたはパスワードが間違っています。");
-            return "login";
-        }
-    }
-	
-	
-	// 本実装
-//	@GetMapping("/logout")
-//	public String logout(HttpSession session) {
-//	    session.invalidate(); // セッションを無効化
-//	    return "redirect:/top"; // トップ画面にリダイレクト
-//	}
-	
-	
-	
-    }
+	@PostMapping("/login-post")
+	public String loginPost(@ModelAttribute LoginForm loginForm, Model model) {
+		// ログイン処理の実行
+		Login login = new Login(loginForm.getUserId(), loginForm.getPass());
+		//        boolean result = loginService.execute(login);
+		Employee employee = loginService.loadAccoundInfo(login);
+
+
+		// ログイン処理の成否によって処理を分岐
+		//        if (result) { // ログイン成功時
+		//            // Modelにユーザー情報を追加
+		//        	model.addAttribute("userId", employee.getUserId());
+		//        	model.addAttribute("name", employee.getName());
+		//        	model.addAttribute("departmentCode", employee.getDepartmentCode());
+		//        	model.addAttribute("numPaidHoliday", employee.getNumPaidHoliday());
+		//            return "top_menu";
+		//        } else { // ログイン失敗時
+		//            model.addAttribute("errorMessage", "ユーザーIDまたはパスワードが間違っています。");
+		//            return "login";
+		//        }
+		if (employee != null) { // ログイン成功時
+			// Modelにユーザー情報を追加
+			model.addAttribute("userId", employee.getUserId());
+			model.addAttribute("name", employee.getName());
+			model.addAttribute("departmentCode", employee.getDepartmentCode());
+			model.addAttribute("numPaidHoliday", employee.getNumPaidHoliday());
+			return "top_menu";
+		} else { // ログイン失敗時
+			model.addAttribute("errorMessage", "ユーザーIDまたはパスワードが間違っています。");
+			return "login";
+		}
+
+	}
+
+}
